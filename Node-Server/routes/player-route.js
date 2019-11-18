052795c968e154254228865420fbc5033b5261b7
FrinkZz (Nganga) <52919747+ngangaNjiraini@users.noreply.github.com>
2019-11-18 17:19:46 +0000

    quick backend update + ERD

--- Node-Server/routes/player-route.js
+++ Node-Server/routes/player-route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const models = require('../models');
+
 const router = express.Router();
 
 router.get('/all', async (_req, res) => {
@@ -23,6 +24,23 @@ router.get('/:index', async (req, res) => {
     } else {
         res.status(404).send({ message: 'Item not found for index ' + req.params.index });
     }
+});
+
+router.get('/searchTag/:name', async (req, res) => {
+    const [first = null] = await models.Player.findAll({
+        where: { playerTag: req.params.name },
+        include: [{ model: models.Region }]
+    });
+    if (first) {
+        res.send(first);
+    } else {
+        res.status(404).send({ message: 'Item not found for index ' + req.params.index });
+    }
+});
+
+router.post('/add', async (req, res) => {
+    await data.push(req.body);
+    res.status(201).send(data);
 });
 
 module.exports = router;
